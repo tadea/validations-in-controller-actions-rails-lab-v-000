6eaf246dc34e0945396026b901069a00fa1980c1
Tadea Simunovic <tsimunoviczg@gmail.com>
2019-03-04 16:11:17 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
         @post.save 
           redirect_to post_path(@post)
         else
-          render :new
+          render :edit
         end
   end
 
